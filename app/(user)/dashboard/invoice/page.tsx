48e82a20f668f44e589ae3d6daba5870f53285e9
Sameer Shamshad <SameerShamshad.42@gmail.com>
2025-11-09 12:21:56 +0500

    Setup invoice route

--- app/(user)/dashboard/invoice/page.tsx
+++ app/(user)/dashboard/invoice/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-const InvoicePage = () => {
+const ManageInvoicePage: React.FC = () => {
+  
+
   return (
     <div>
-      Invoices
+      {/* Header */}
+      <header className="flex justify-between items-center px-5 py-5">
+        <h1 className="text-2xl font-bold text-main">Invoices</h1>
+
+        <Button>Create Invoice</Button>
+      </header>
+
+      <section className=" border-b">
+
+      </section>
     </div>
   );
-}
+};
 
-export default InvoicePage;
+export default ManageInvoicePage;
